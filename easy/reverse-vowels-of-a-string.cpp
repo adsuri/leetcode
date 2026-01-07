aba928d768a8e816ead9c6b60b85a19040264899
Aditya Suri <aditya.as.suri@gmail.com>
2026-01-06 21:15:45 -0500

    _

--- easy/reverse-vowels-of-a-string.cpp
+++ easy/reverse-vowels-of-a-string.cpp
@@ -3,37 +3,37 @@
 
 
 bool is_vowel(char s) {
-        return s == 'a'
-            || s == 'e'
-            || s == 'i'
-            || s == 'o'
-            || s == 'u'
-            || s == 'A'
-            || s == 'E'
-            || s == 'I'
-            || s == 'O'
-            || s == 'U';
-    }
+  return s == 'a'
+      || s == 'e'
+      || s == 'i'
+      || s == 'o'
+      || s == 'u'
+      || s == 'A'
+      || s == 'E'
+      || s == 'I'
+      || s == 'O'
+      || s == 'U';
+}
 
 class Solution {
  public:
-    std::string reverseVowels(std::string s) {
-        int left = 0;
-        int right = s.length() - 1;
+  std::string reverseVowels(std::string s) {
+    int left = 0;
+    int right = s.length() - 1;
 
-        while (right > left) {
-            if (!is_vowel(s[left])) {
-                ++left;
-            } else if (!is_vowel(s[right])) {
-                --right;
-            } else {
-                char temp = s[left];
-                s[left] = s[right];
-                s[right] = temp;
-                ++left;
-                --right;
-            }
-        }
-        return s;
+    while (right > left) {
+      if (!is_vowel(s[left])) {
+        ++left;
+      } else if (!is_vowel(s[right])) {
+        --right;
+      } else {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        ++left;
+        --right;
+      }
     }
+    return s;
+  }
 };
