78dc0ad02cb9419439df8c11a2bbd09473c9b910
Aditya Suri <aditya.as.suri@gmail.com>
2026-01-06 20:55:49 -0500

    Leetcode 345

---
+++ easy/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,39 @@
+// Leetcode #345: Reverse Vowels of a String - Easy
+#include <string>
+
+
+bool is_vowel(char s) {
+        return s == 'a'
+            || s == 'e'
+            || s == 'i'
+            || s == 'o'
+            || s == 'u'
+            || s == 'A'
+            || s == 'E'
+            || s == 'I'
+            || s == 'O'
+            || s == 'U';
+    }
+
+class Solution {
+public:
+    std::string reverseVowels(std::string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (right > left) {
+            if (!is_vowel(s[left])) {
+                ++left;
+            } else if (!is_vowel(s[right])) {
+                --right;
+            } else {
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                ++left;
+                --right;
+            }
+        }
+        return s;
+    }
+};
